e723f92bb3e37df49bbd5ff3ddf46c763c1a74de
fyxdev <fyx.dev@gmail.com>
2020-11-18 21:54:36 +0800

    change the web

--- scripts.js
+++
@@ -1,6 +0,0 @@
-//Create function to select elements
-const selectElement = (element) => document.querySelector(element);
-//Open and close nav on click
-selectElement(element: '.menu-icons')addEventListener(type: 'click', listener: () => {
-    selectElement(element: 'nav') .classList.toggle( token: 'active');
-});
